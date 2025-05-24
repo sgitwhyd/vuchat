47a524f7ef756232998d5f66544e931fcf077802
sgitwhyd <sigitwahyu.dev@gmail.com>
2025-05-24 15:56:55 +0700

    feat: Add chat and room stores using Pinia for state management

---
+++ src/stores/chat_store.ts
@@ -0,0 +1,59 @@
+import { generateDummyChat } from '@/utils/utils'
+import { defineStore } from 'pinia'
+import {type Chat} from '@/utils/utils'
+
+
+interface User {
+  id: number
+  name: string
+  avatar: string
+}
+
+const CHAT_STORAGE_KEY = 'chats'
+import router from '@/router'
+
+export const useChatStore = defineStore('chat', {
+  state: () => ({
+    messages: [] as Chat[],
+    isLoading: false,
+  }),
+
+  getters: {
+    getIsLoading: (state) => state.isLoading,
+  },
+
+  actions: {
+    setMessage() {
+      if (localStorage.getItem(CHAT_STORAGE_KEY)) {
+        this.messages = JSON.parse(localStorage.getItem(CHAT_STORAGE_KEY) || '[]')
+      } else {
+        const dummyChat = generateDummyChat()
+        this.messages = dummyChat
+        localStorage.setItem(CHAT_STORAGE_KEY, JSON.stringify(dummyChat))
+      }
+    },
+
+    getMessage(roomId: string) {
+      const messages = this.messages.filter((message) => message.room_id === roomId)
+      if(messages.length === 0) {
+       router.push('/not-found')
+      }
+      return messages
+    },
+
+    addMessage(content: string, roomId: string, userId: string | null) {
+
+      const newMessage: Chat = {
+        id: new Date().getTime(),
+        room_id: roomId,
+        message: content,
+        is_sender: true,
+        user_id: userId,
+        user_avatar_url: null,
+        created_at: new Date().toISOString()
+      }
+      this.messages.push(newMessage)
+      localStorage.setItem(CHAT_STORAGE_KEY, JSON.stringify(this.messages))
+    }
+  }
+}) 
---
+++ src/stores/room_store.ts
@@ -0,0 +1,57 @@
+
+import { defineStore } from "pinia"
+
+export interface RoomStoreState {
+    id: string
+    name: string
+    user_avatar_url: string
+  }
+
+  export interface Room {
+    channel_id: number;
+    contact_id: number | null;
+    id: number;
+    is_calling: boolean;
+    is_handled_by_bot: boolean;
+    is_resolved: boolean;
+    is_waiting: boolean;
+    last_comment_sender: string;
+    last_comment_sender_type: string;
+    last_comment_text: string;
+    last_comment_timestamp: string;
+    last_customer_comment_text: string | null;
+    last_customer_timestamp: string;
+    name: string;
+    room_badge: string | null;
+    room_id: string;
+    room_type: string;
+    source: string;
+    user_avatar_url: string;
+    user_id: string;
+}
+
+const ROOM_KEY = 'room'
+
+export const useRoomStore = defineStore('room', {
+  state: () => ({
+    room: null as RoomStoreState | null,
+    rooms: [] as Room[]
+  }),
+  getters:{
+    getRoom: (state) => state.room
+  },
+
+  actions: {
+    setRoom(room: RoomStoreState) {
+      this.room = room
+    },
+    setRooms(rooms: Room[]) {
+      this.rooms = rooms
+    },
+    clearRoom() {
+      this.room = null
+    }
+  },
+  persist: true
+})
+
